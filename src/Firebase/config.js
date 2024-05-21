e28a1a76af560acfefc1e49c166daee61890afb2
Octavio <lombardioctavio@gmail.com>
2024-05-21 10:51:39 -0300

    Hosting update

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -6,9 +6,16 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAK_I-xp_EGqhcy6X0R4qlQ_-iZt1oVJTk',
+  authDomain: 'portfolio-a2100.firebaseapp.com',
+  projectId: 'portfolio-a2100',
+  storageBucket: 'portfolio-a2100.appspot.com',
+  messagingSenderId: '861195749348',
+  appId: '1:861195749348:web:8a39a3c567d66c40aca9f1',
+  measurementId: 'G-GWV9G63DL9',
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-//firebase deploy --only hosting:octaviolombardidev
-/* "site": "octaviolombardidev",*/
